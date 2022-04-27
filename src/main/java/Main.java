50fbff37cfa76c0c78174f43c8de0b4384dcc6fd
Бродников Денис <platz198@yandex.ru>
2022-04-27 14:08:07 +0700

    "update Main"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,21 +8,19 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
+//        System.out.println(engine.search("бизнес"));
 
         int port = 8989;
 
-        while (true) {
-            try (ServerSocket serverSocket = new ServerSocket(port);
-                 Socket clientSocket = serverSocket.accept();
-                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-                Gson gson = new Gson();
-                String word = in.readLine();
-                out.println(gson.toJson(engine.search(word)));
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket clientSocket = serverSocket.accept();
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            Gson gson = new Gson();
+            String word = in.readLine();
+            out.println(gson.toJson(engine.search(word)));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
